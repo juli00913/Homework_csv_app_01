1b5ae76ebc04b9ddf89b9e6d9fa0178aaf1dbc7e
July Slavchev <jul.slavhev.tues19@gmail.com>
2017-10-10 21:06:51 +0300

    init

---
+++ app/controllers/sums_controller.rb
@@ -0,0 +1,21 @@
+
+require 'csv'
+
+class SumsController < ApplicationController
+	def index
+	end
+	
+	def create
+	 csv_file = params[:csv_file]
+	 csv_file_path = csv_file.path
+	 
+	 result = 0;
+	 CSV.foreach(csv_file_path) do |row|
+		result += row[0].to_f
+		end
+		
+	 respond_to do |format|
+	 	format.html { render plain: result.ceil(2)}
+		end 
+	end
+end
