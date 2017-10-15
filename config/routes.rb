76e2b9ae9133cbdf9414790ffd6d348c3622a002
July Slavchev <jul.slavhev.tues19@gmail.com>
2017-10-15 16:18:58 +0300

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
 	get 'sums' => 'sums#index'
 	post 'sums' => 'sums#create'
-	root 'sums#index'
 	
 	get 'files' => 'filters#index'
 	post 'files' => 'filters#filtering'
-	root 'files#index'
 end
